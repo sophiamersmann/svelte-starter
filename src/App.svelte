7279d3204d51f29e84a113c86d7d6e3abbf1a669
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-07-24 08:37:37 +0200

    :fire: Remove assets folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,8 @@
 <script>
-  import logo from "./assets/svelte.png";
   import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
   <h1>Hello world!</h1>
 
   <Counter />
@@ -30,11 +28,6 @@
     margin: 0 auto;
     padding: 1em;
     text-align: center;
-  }
-
-  img {
-    width: 16rem;
-    height: 16rem;
   }
 
   h1 {
