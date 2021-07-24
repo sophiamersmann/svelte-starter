a613964a76c5b5c3edd409370ec0bc37c9a1181d
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-07-24 08:39:12 +0200

    :art: Use single quotes

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Counter from "./lib/Counter.svelte";
+  import Counter from './lib/Counter.svelte';
 </script>
 
 <main>
@@ -20,8 +20,8 @@
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   main {
