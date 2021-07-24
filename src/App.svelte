b73ac8804b6b3cd61e64d32ce8a230e8abbc4c48
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-07-24 09:11:41 +0200

    :fire: Remove some example code

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,7 @@
 
 <main>
   <h1>Hello world!</h1>
-
   <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style>
@@ -40,18 +29,8 @@
     text-transform: uppercase;
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
   @media (min-width: 480px) {
     h1 {
-      max-width: none;
-    }
-
-    p {
       max-width: none;
     }
   }
